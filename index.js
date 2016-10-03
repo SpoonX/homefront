04fabcd955252cfa2a0d99095f7148030a62e8b2
RWOverdijk <r.w.overdijk@gmail.com>
2016-10-03 23:06:44 +0200

    fix(project): Check if null before attempting remove

--- index.js
+++ index.js
@@ -252,7 +252,7 @@ class Homefront {
     let lastKey       = normalizedKey.pop();
     let source        = this.fetch(normalizedKey);
 
-    if (typeof source === 'object' && typeof source[lastKey] !== 'undefined') {
+    if (typeof source === 'object' && source !== null) {
       delete source[lastKey];
     }
 
